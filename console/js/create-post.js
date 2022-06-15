0f5daf3061e2062789f67df469c8535e20b40fe5
Franco-H <francohui@berkeley.edu>
2022-06-15 11:10:51 -0700

    Create article function at frontend

--- console/js/create-post.js
+++ console/js/create-post.js
@@ -1,10 +1,12 @@
-let createForm = document.querySelector('.create-post-form');
+let createForm = document.querySelector('.create-article-form');
+
 let destination = document.querySelector('#destination');
 let location = document.querySelector('#location');
+let image = document.querySelector('#image');
 let description = document.querySelector('#description');
 
 
-createForm.addEventListener('submit', function(e) {
+createForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
     let data = new FormData();
@@ -13,9 +15,17 @@ createForm.addEventListener('submit', function(e) {
     data.append('description', createdescription);
     data.append('description', description.value);
 
-    fetch('http://localhost:3000/posts', {
+    fetch('https://destination-blogger.herokuapp.com/posts', {
         method: 'POST',
-        body: data
-    }).then((res) => res.description()).then((data) => window.history.go());
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            destination: destination.value,
+            location: location.value,
+            description: description.value,
+            image: image.value,
 
+        })
+    }).then(res => res.text()).then(data => console.log(data));
 });
