4f1f8928d9bcfbc48307cf87bdaaddbc7817ed6d
Franco-H <francohui@berkeley.edu>
2022-06-15 11:53:36 -0700

    Create Post needs fix

--- console/js/create-post.js
+++ console/js/create-post.js
@@ -5,15 +5,8 @@ let location = document.querySelector('#location');
 let image = document.querySelector('#image');
 let description = document.querySelector('#description');
 
-
-createForm.addEventListener('submit', (e) => {
+createForm.addEventListener('submit', function(e) {
     e.preventDefault();
-
-    let data = new FormData();
-    data.append('destination', destination.value);
-    data.append('location', location.value);
-    data.append('description', createdescription);
-    data.append('description', description.value);
 
     fetch('https://destination-blogger.herokuapp.com/posts', {
         method: 'POST',
@@ -25,7 +18,6 @@ createForm.addEventListener('submit', (e) => {
             location: location.value,
             description: description.value,
             image: image.value,
-
         })
     }).then(res => res.text()).then(data => console.log(data));
 });
